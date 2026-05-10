ef7989c6376faa6850b74ad2f618438472de1fc1
Hobo <dasfisky@gmail.com>
2026-05-10 23:44:46 +0200

    Create yolo_training.py

---
+++ yolo_training.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+if __name__ == "__main__":
+    model = YOLO("yolo26n")  
+
+    results = model.train(
+        data="config.yaml",          
+        epochs=150,
+        imgsz=320,                   
+        batch=0.85,                    
+        device=0,                    
+        patience=50,                 
+        project="runs/train",
+        name="car_detector_320",
+        exist_ok=True
+    )
